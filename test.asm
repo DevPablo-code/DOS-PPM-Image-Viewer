ab18cbf91c3f540ef20b623cd63fd701f716ca85
DevPablo <anenub4@gmail.com>
2023-11-01 15:38:20 +0200

    StringToNumber test

--- test.asm
+++ test.asm
@@ -1,1 +1,33 @@
-add ax, cx
+org 100h
+
+mov si, &NumberString
+call StringToNumber
+
+ret
+
+NumberString: db '128' db 0
+
+StringToNumber:
+        push bx
+        push cx
+
+        xor ax, ax
+        xor cx, cx
+
+        mov bx, 10
+    .Loop:
+        mov cl, [si]
+        cmp cl, 0
+        jz .Exit
+
+        sub cx, 30h
+        mul bx
+        add ax, cx
+
+        inc si
+        jmp .Loop
+        
+    .Exit:
+        pop cx
+        pop bx
+        ret
